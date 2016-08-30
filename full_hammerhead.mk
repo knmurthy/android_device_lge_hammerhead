8db638b5a06e613a22231bf5ce896e27b413f40f
Yashar Ahmedizade <yashade2001@gmail.com>
2016-08-30 13:40:01 +0000

    update vendor path

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -31,6 +31,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/lge/hammerhead/device.mk)
-$(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/hammerhead/hammerhead-vendor.mk)
 
 TARGET_NEEDS_TEXT_RELOCATIONS := true
